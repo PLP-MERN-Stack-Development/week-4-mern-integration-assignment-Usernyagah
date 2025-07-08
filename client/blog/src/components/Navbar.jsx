ae0d133a79e6f00bffcd6ffc0af19a0f9dc1f607
Usernyagah <developernyagah@gmail.com>
2025-07-08 13:29:41 +0300

    Add ModeToggle and ThemeProvider components for theme management

---
+++ client/blog/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import ModeToggle from './ModeToggle';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white dark:bg-gray-900 shadow px-6 py-4 mb-6 flex justify-between items-center">
+      <Link to="/" className="text-xl font-bold text-black dark:text-white">MERN Blog</Link>
+      <div className="space-x-2 flex items-center">
+        <Button asChild><Link to="/">Home</Link></Button>
+        <Button asChild><Link to="/create">Create Post</Link></Button>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
